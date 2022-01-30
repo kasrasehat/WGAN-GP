7d10cea360aef23ddf4607856a6fadb7a7100163
kasra sehat <kasra.sehat@yahoo.com>
2022-01-30 13:41:58 +0330

    create generator and critic

---
+++ main.py
@@ -0,0 +1,43 @@
+import torch
+from torch import nn
+from tqdm.auto import tqdm
+from torchvision import transforms
+from torchvision.datasets import MNIST
+from torchvision.utils import make_grid
+from torch.utils.data import DataLoader
+import matplotlib.pyplot as plt
+torch.manual_seed(0) # Set for testing purposes, please do not change!
+from Networks import Generator, Critic
+
+def show_tensor_images(image_tensor, num_images=25, size=(1, 28, 28)):
+    '''
+    Function for visualizing images: Given a tensor of images, number of images, and
+    size per image, plots and prints the images in an uniform grid.
+    '''
+    image_tensor = (image_tensor + 1) / 2
+    image_unflat = image_tensor.detach().cpu()
+    image_grid = make_grid(image_unflat[:num_images], nrow=5)
+    plt.imshow(image_grid.permute(1, 2, 0).squeeze())
+    plt.show()
+
+def make_grad_hook():
+    '''
+    Function to keep track of gradients for visualization purposes,
+    which fills the grads list when using model.apply(grad_hook).
+    '''
+    grads = []
+    def grad_hook(m):
+        if isinstance(m, nn.Conv2d) or isinstance(m, nn.ConvTranspose2d):
+            grads.append(m.weight.grad)
+    return grads, grad_hook
+
+def get_noise(n_samples, z_dim, device='cpu'):
+    '''
+    Function for creating noise vectors: Given the dimensions (n_samples, z_dim)
+    creates a tensor of that shape filled with random numbers from the normal distribution.
+    Parameters:
+      n_samples: the number of samples to generate, a scalar
+      z_dim: the dimension of the noise vector, a scalar
+      device: the device type
+    '''
+    return torch.randn(n_samples, z_dim, device=device)
